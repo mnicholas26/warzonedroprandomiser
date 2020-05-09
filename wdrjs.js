3424985668d1c04fc2fb94960f44dba9a3177e2f
mnicholas26 <spiritbeast26@yahoo.co.uk>
2020-05-09 23:48:30 +0100

    basic deadzone implementation

--- wdrjs.js
+++ wdrjs.js
@@ -5,9 +5,28 @@ window.onload = function()
     var ns = "http://www.w3.org/2000/svg";
     function randomsquare()
     {
+        let deadzones = [
+            {x: 2, y: 1},
+            {x: 2, y: 8},
+            {x: 5, y: 1},
+            {x: 6, y: 1},
+            {x: 7, y: 1},
+            {x: 8, y: 1},
+            {x: 8, y: 2},
+            {x: 8, y: 3},
+            {x: 8, y: 4}
+        ]
         let currect = document.getElementById("dropsquare");
-        let x = 8 + 98.4*Math.floor(Math.random()*10);
-        let y = 8 + 98.4*Math.floor(Math.random()*10);
+        let deadzone = true;
+        let row, col = 0;
+        while(deadzone)
+        {
+            row = 1 + Math.floor(Math.random()*8);
+            col = 2 + Math.floor(Math.random()*7);
+            if(!deadzones.map(x => x.x).includes(row) && !deadzones.map(x => x.y).includes(col)) deadzone = false;
+        }
+        let x = 8 + 98.4*row;
+        let y = 8 + 98.4*col;
 
         let square = document.createElementNS(ns, "rect");
         square.setAttribute('width', 97.4);
