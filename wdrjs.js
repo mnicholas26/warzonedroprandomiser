f99eb7350be963b072d51214b12a48d372d38d74
mnicholas26 <spiritbeast26@yahoo.co.uk>
2020-05-10 16:18:04 +0100

    better cell method and improved css

--- wdrjs.js
+++ wdrjs.js
@@ -9,100 +9,118 @@ window.onload = function()
     {
         let method = {};
         method.name = 'Map Cell';
-        //method.squares = [...Array(64).keys()];
-        method.displaydeadzones = {};
-        method.displaydeadzones.value = false;
-        method.displaydeadzones.toggle = () => {
-            method.displaydeadzones.value = !method.displaydeadzones.value;
-            let squares = document.querySelectorAll('.deadzone');
-            for(let i = 0; i < squares.length; i++)
-            {
-                squares[i].classList.toggle('hidden');
+        method.cells = {
+            domcells: [],
+            swap: function (cell){
+                cell.deadzone = !cell.deadzone;
+                cell.marker = !cell.marker
+            },
+            visible: false,
+            toggle: () => {
+                method.cells.visible = !method.cells.visible;
+                for(let i = 0, cells = method.cells.domcells; i < cells.length; i++)
+                {
+                    if(!cells[i].marker) cells[i].toggle('hidden');
+                }
             }
         }
-        method.deadzones = [];
         method.settings = {};
         method.animations = [];
+
+        function shuffle(array)
+        {
+            var currentIndex = array.length, temporaryValue, randomIndex;
+            // While there remain elements to shuffle...
+            while (0 !== currentIndex) {
+              // Pick a remaining element...
+              randomIndex = Math.floor(Math.random() * currentIndex);
+              currentIndex -= 1;
+              // And swap it with the current element.
+              temporaryValue = array[currentIndex];
+              array[currentIndex] = array[randomIndex];
+              array[randomIndex] = temporaryValue;
+            }
+            return array;
+        }
 
         method.random = function randomsquare()
         {
             let mapmarkers = document.querySelectorAll(".marker");
-            let deadzone = true;
-            let row, col = 0;
-            while(deadzone)
+            let possiblecells = method.cells.domcells.filter((item) => {
+                return !item.deadzone;
+            });
+            if(possiblecells.length <= 0)
             {
-                row = 1 + Math.floor(Math.random()*8);
-                col = 2 + Math.floor(Math.random()*7);
-                if(!method.deadzones.map(x => x.x + (x.y*10)).includes((row*10)+col)) deadzone = false;
+                alert("No Possible Dropzone, please adjust deadzones");
+                return;
             }
-            let x = 8 + 98.4*col;
-            let y = 8 + 98.4*row;
-
-            let marker = document.createElementNS(ns, "rect");
-            marker.setAttribute('width', 97.4);
-            marker.setAttribute('height', 97.4);
-            marker.setAttribute('x', x);
-            marker.setAttribute('y', y);
-            marker.setAttribute('fill', "rgba(255,298,10,0.2)");
-            marker.setAttribute('stroke', "rgb(255,198,10)");
-            marker.setAttribute('stroke-width', 2);
-            marker.setAttribute('class', "marker");
+            shuffle(possiblecells);
+            //can choose multiple later
+            for(i = 0, amount = 1; i < amount; i++)
+            {
+                let cell= possiblecells[i];
+                cell.toggle('marker');
+                cell.off('hidden');
+            }
 
             if(mapmarkers != undefined)
             {
                 for(let i = 0; i < mapmarkers.length; i++)
                 {
-                    svg.removeChild(mapmarkers[i]);
+                    mapmarkers[i].toggle('marker');
+                    if(!method.cells.visible) mapmarkers[i].on('hidden');
                 }
             }
-            svg.appendChild(marker);
         }
 
         //add animations to my random function, my defaults and as a property of the object
 
         method.defaults = {
-            deadzones: [
-                {x: 2, y: 1},
-                {x: 2, y: 8},
-                {x: 5, y: 1},
-                {x: 6, y: 1},
-                {x: 7, y: 1},
-                {x: 8, y: 1},
-                {x: 8, y: 2},
-                {x: 8, y: 3},
-                {x: 8, y: 4}
-            ],
+            deadzones: [0,1,4,5,6,7,8,15,16,23,24,31,32,40,48,56,57],
             settings: {
                 granularity: 1
             }
         }
 
-        method.deadzones = method.defaults.deadzones;
         method.settings = method.defaults.settings;
 
-        method.drawdeadzones = () =>
-        {
-            for(let i = 0; i < method.deadzones.length; i++)
+        method.makescells = () => {
+            for(let i = 0; i < 64; i++)
             {
-                let deadzone = document.createElementNS(ns, "rect");
-                deadzone.setAttribute('width', 97.4);
-                deadzone.setAttribute('height', 97.4);
-                deadzone.setAttribute('x', (method.deadzones[i].x*98.4) + 8);
-                deadzone.setAttribute('y', (method.deadzones[i].y*98.4) + 8);
-                deadzone.setAttribute('fill', "rgba(204,0,0,0.2)");
-                deadzone.setAttribute('stroke', "rgb(204,0,0)");
-                deadzone.setAttribute('stroke-width', 2);
-                deadzone.setAttribute('class', "deadzone hidden");
-                svgarea.appendChild(deadzone);
+                let x = (((i%8)+1)*98.4)+8;
+                let y = ((Math.floor(i/8)+1)*98.4)+8;
+                let cell = document.createElementNS(ns, "rect");
+                cell.setAttribute('width', 97.4);
+                cell.setAttribute('height', 97.4);
+                cell.setAttribute('x', x);
+                cell.setAttribute('y', y);
+                cell.setAttribute('class', 'hidden cell');
+                cell.deadzone = false;
+                cell.marker = false;
+                cell.hidden = false;
+                cell.toggle = (prop) => {
+                    cell[prop] = !cell[prop]
+                    cell.classList.toggle(prop);
+                }
+                cell.on = (prop) => {
+                    cell[prop] = true;
+                    cell.classList.add(prop);
+                }
+                cell.off = (prop) => {
+                    cell[prop] = false;
+                    cell.classList.remove(prop);
+                }
+                cell.addEventListener('click', () => {
+                    cell.toggle("deadzone");
+                    cell.off('marker');
+                })
+                svg.appendChild(cell);
+                if(method.defaults.deadzones.includes(i)) cell.toggle("deadzone");
+                method.cells.domcells.push(cell);
             }
         }
 
-        method.drawdeadzones();
-
-        method.dom = document.getElementById('methodsquare');
-        method.show = () => {method.dom.classList.remove('hidden');}
-        method.hide = () => {method.dom.classList.add('hidden');}
-        method.toggle = () => {method.dom.classList.toggle('hidden');}
+        method.makescells();
         return method;
     }
 
@@ -183,7 +201,7 @@ window.onload = function()
     {
         toggles[i].addEventListener('mousedown', () => {
             toggles[i].classList.toggle('checked');
-            currentmethod.displaydeadzones.toggle()
+            currentmethod.cells.toggle()
         });
     }
 
